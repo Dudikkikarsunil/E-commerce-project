57ce45ad708912260fceeadcdf651db080ebe6c7
prince practicesunil <princepracticesunil07@gmail.com>
2024-02-26 20:35:00 +0530

    files added

---
+++ src/FireBase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCshH1Um8B8oRJbY9HLvsQfBdprKOCm-Uc",
+    authDomain: "project-react-3263f.firebaseapp.com",
+    projectId: "project-react-3263f",
+    storageBucket: "project-react-3263f.appspot.com",
+    messagingSenderId: "739776494589",
+    appId: "1:739776494589:web:5b1eaf13e2662ede8d7794",
+    measurementId: "G-NDRK5055BM"
+  };
